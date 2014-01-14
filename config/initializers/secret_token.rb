259788e6e2c9fddc50e9a13a5912536aae5bdf3d
Emmanuel Valle <emmanuel.valle@akarisoft.com>
2014-01-13 23:19:09 -0600

    Fixing development gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '4999a248f8a308e249bf349e9a2cff2631185c56de8de18963a9308e5391598cab791ef23b1704c94df802567bcd958dd306ad98cd71cee130d4f3ca66eea63a'
